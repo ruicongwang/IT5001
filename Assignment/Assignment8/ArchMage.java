7d07d93e0af450b8948aaa87975aa3a8c2d84bf3
ruicongwang <122773468+ruicongwang@users.noreply.github.com>
2023-01-16 15:40:50 +0800

    Add files via upload

---
+++ Assignment/Assignment8/ArchMage.java
@@ -0,0 +1,25 @@
+/** An arch mage */
+public class ArchMage extends Mage {
+    // public constants
+    public static final int COST = 600;
+
+    // private constants
+    private static final String ARCHMAGE_NAME = "ArchMage";
+    private static final String KABOOM_STRING = "Cast KABOOM! Strike every enemy!";
+    private static final int KABOOM_MULTIPLIER = 2;
+    public ArchMage() {
+        super(ARCHMAGE_NAME);
+    }
+
+    @Override
+    public void cast(Team myTeam, Team enemyTeam) {
+        if (myTeam.getAllLivingCharacters().length == 1) {
+            System.out.println(KABOOM_STRING);
+            for (GameCharacter targets : enemyTeam.getAllLivingCharacters()){
+                targets.gotHurt(intelligence * KABOOM_MULTIPLIER);
+            }
+        } else {
+            super.cast(myTeam, enemyTeam);
+        }
+    }
+}
